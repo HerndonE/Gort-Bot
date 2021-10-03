d7603e82e90558296647f3fbe995c5d16a524d04
HerndonE <EHerndon17@gmail.com>
2021-10-03 11:13:06 -0700

    [update] helper function

--- Gort-BotFiles/helpers/helper.js
+++ Gort-BotFiles/helpers/helper.js
@@ -1,28 +1,26 @@
 //https://stackoverflow.com/questions/45932650/how-to-check-if-a-url-or-a-webservice-is-alive-in-nodejs
-var link = "http://taco-randomizer.herokuapp.com/random/?full-taco=true";
+const request = require('request');
 
-request(link , function (error, response, body) {
+function checkurl(url){
+request(url , function (error, response, body) {
 
   if(error){
-    console.log('Err: '+ error);
-    return false;
+    console.log('Error: '+ error);
+    return;
   }
+  
+  if(response.statusCode >= 400) {//Numbers are set accordingly to Status Codes: https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+    console.log('API: ' + url + ' has a status code of ' + response.statusCode + " Status:❌" );
+    return;
 
-if(response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202){
-  console.log(link + ' is up!!');
-  return false;
 }
-
-if(response.statusCode == 301 || response.statusCode == 302){
-  console.log(link + ' is redirecting us!!');
-  return false;
+else {
+  //console.log('API: ' + url + ' has a status code of ' + response.statusCode +" Status:✔️");
+  //return;
 }
-
-if(response.statusCode == 401){
-  console.log("you are unauthorized to " + link);
-  return false;
-}else{
-  console.log(link + ' is down!!');
-}
-
+ 
 });
+
+}
+
+exports.checkurl = checkurl;
